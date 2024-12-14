3ff6ffc4c7a64f246285b8d2df21b2b1cad0f45e
Jer403 <jeremiwtfxd@gmail.com>
2024-12-13 22:54:57 -0500

    fix click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,21 +47,21 @@ export function Header() {
           <button className='switch-mobile' onClick={handlerClick}>{icon}</button>
           <div className="flex mobile-links gap-6">
             <div className="flex flex-col gap-6 links-t">
-              <a href="#skills" className="hover:text-blue-400 transition-colors">Skills</a>
-              <a href="#projects" className="hover:text-blue-400 transition-colors">Projects</a>
-              <a href="#contact" className="hover:text-blue-400 transition-colors">Contact</a>
+              <a href="#skills" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>Skills</a>
+              <a href="#projects" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>Projects</a>
+              <a href="#contact" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>Contact</a>
             </div>
             <div className="flex items-center gap-4 links-i">
-              <a href="mailto:josefelixlr05@gmail.com" className="hover:text-blue-400 transition-colors">
+              <a href="mailto:josefelixlr05@gmail.com" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>
                 <Mail className="w-5 h-5" />
               </a>
-              <a href="https://github.com/josefelix" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors">
+              <a href="https://github.com/josefelix" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>
                 <Github className="w-5 h-5" />
               </a>
-              <a href="https://cu.linkedin.com/in/jos%C3%A9-felix-lorenzo-rodr%C3%ADguez-b747b6328" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors">
+              <a href="https://cu.linkedin.com/in/jos%C3%A9-felix-lorenzo-rodr%C3%ADguez-b747b6328" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>
                 <Linkedin className="w-5 h-5" />
               </a>
-              <a href="https://www.facebook.com/profile.php?id=100088139118774" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors">
+              <a href="https://www.facebook.com/profile.php?id=100088139118774" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition-colors" onClick={handlerClick}>
                 <Facebook className="w-5 h-5" />
               </a>
             </div>
